979721aa413ba37f39c2a562abe98c74611de887
strutive07 <strutive07@gmail.com>
2018-08-29 01:20:09 +0900

    error

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {'error':error});
 });
 var SocketIo = require('socket.io');
 const socketEvent = require('./functions/chat');
